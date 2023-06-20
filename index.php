4a1fdcd7f7836abe535d2676da05178a7fd62e44
ManuelF <manuel.floriani1999@gmail.com>
2023-06-20 19:42:55 +0200

    Creazione tabella Con elementi dell'array

--- index.php
+++ index.php
@@ -48,10 +48,49 @@ $hotels = [
         ],
 
     ];
+    ?>
 
+   <table class="table table-dark">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <?php
+            //Stampa ogni chiave del primo elemento nell'array hotels
+            foreach ($hotels[0] as $key => $value) {               
+                echo "<th scope='col'>" . $key . "</th>";
+            };
+        ?>  
+      </tr>
+    </thead>
+    <tbody>
+    <?php
+                // Per ogni hotel nella lista
+                foreach ($hotels as $k => $hotel) {
+                    // Crea una riga della colonna
+                    echo "<tr>";
+                    // Ed aumenta il valore ciclato 'k' di 1
+                    echo "<th scope='row'>" . $k . "</th>";
+                    // Per ogni hotel nella lista
+                    foreach ($hotel as $key => $value) {
+                        // Se alla chiave parking è assegnato valore true
+                        if ($key == 'parking' && $value == true) {
+                            // Sostituisco quel valore con una spunta
+                            $value = '&#x2713;';
+                        }
+                        // Se alla chiave parking è assegnato valore false
+                        else if ($key == 'parking' && $value == false){
+                            // Sostituisco quel valore con una X
+                            $value = '&#10005;';
+                        }
+                        echo "<td>" . $value . "</td>";
+                    }
+                    echo "</tr>";
+                }
+            ?>
+    </tbody>
+  </table> 
 
-?>    
-
+    
 
 
 </body>
